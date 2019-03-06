ad0d6c4a30ef76b985c532a4af37060a8b70e7de
skazure <kumarmit@me.com>
2019-03-06 23:27:06 +0100

    new component and modules added

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { EnvironmentUrlService } from './shared/services/environment-url.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { RouterModule } from '@angular/router';
+import { OwnerListComponent } from './owner/owner-list/owner-list.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OwnerListComponent,
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UiModule
+    UiModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'owner', loadChildren: "./owner/owner.module#OwnerModule" },
+      { path: '404', component : NotFoundComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full'}      
+     ])    
   ],
-  providers: [],
+  providers: [EnvironmentUrlService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
